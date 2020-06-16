eda5769189163f1a5da6aa89abd86625b2e2d44c
Luke Murray <lukepigeonmail@gmail.com>
2020-06-15 23:23:02 -0400

    Fix bugs in rename provider. Fixes #42.

--- src/features/MarkdownRenameProvider.ts
+++ src/features/MarkdownRenameProvider.ts
@@ -1,19 +1,20 @@
 import fs from "fs";
 import path from "path";
 import * as vscode from "vscode";
+import { isCitationKeyFileReference } from "../core/common/typeGuards";
 import { fileReferenceContentRange } from "../core/fileReference/fileReferenceContentRange";
-import { titleToBasename } from "../core/fileReference/fileReferenceFsPath";
+import {
+  fileReferenceFsPath,
+  titleToBasename,
+} from "../core/fileReference/fileReferenceFsPath";
 import { positionFileReference } from "../core/fileReference/positionFileReference";
 import { fsPathBacklinkFileReferences } from "../core/fsPath/fsPathBacklinkFileReferences";
-import { textDocumentFsPath } from "../core/fsPath/textDocumentFsPath";
 import { uriFsPath } from "../core/fsPath/uriFsPath";
 import {
   selectLinkedFileFsPaths,
-  waitForLinkedFileToUpdate,
   waitForAllLinkedFilesToUpdate,
 } from "../reducers/linkedFiles";
 import { LinkedNotesStore } from "../store";
-import { isCitationKeyFileReference } from "../core/common/typeGuards";
 
 class MarkdownRenameProvider implements vscode.RenameProvider {
   private store: LinkedNotesStore;
@@ -49,58 +50,62 @@ class MarkdownRenameProvider implements vscode.RenameProvider {
       return undefined;
     }
 
-    // get the referenced document
+    // get the file reference under the caret
     let ref = positionFileReference(position, document, this.store);
     if (ref !== undefined && !isCitationKeyFileReference(ref)) {
-      // get the id of the referenced document
-      const originalFsPath = textDocumentFsPath(document);
-      const backLinks = fsPathBacklinkFileReferences(
-        originalFsPath,
-        this.store
-      );
-      // create a new workspace edit to apply
-      const workspaceEdit = new vscode.WorkspaceEdit();
-      for (let backLink of backLinks) {
-        const contentRange = fileReferenceContentRange(backLink);
-        if (contentRange === undefined) {
-          continue;
-        }
-        workspaceEdit.replace(
-          vscode.Uri.file(backLink.sourceFsPath),
-          contentRange,
-          newName
+      // get the referenced file
+      const refTargetFsPath = fileReferenceFsPath(ref, this.store);
+      if (refTargetFsPath !== undefined) {
+        // get backlinks to the referenced file
+        const backLinks = fsPathBacklinkFileReferences(
+          refTargetFsPath,
+          this.store
         );
-      }
-      // rename the file
-      // get the new uri
-      const newUri = vscode.Uri.file(
-        path.resolve(originalFsPath, "..", titleToBasename(newName))
-      );
-      // only rename if the file is going to a new place
-      // this can occur if we change the reference name in such a way that
-      // the slugged version normalizes to the same file
-      if (newUri.fsPath !== originalFsPath) {
-        // check if the uri already exists
-        // can occur if the user is trying to rename all instances of one reference
-        // to another reference
-        const existingFsPaths = new Set(
-          selectLinkedFileFsPaths(this.store.getState())
-        );
-        const newFsPath = uriFsPath(newUri);
-        // throw an error if the user is merging references (not sure how to support)
-        if (existingFsPaths.has(newFsPath)) {
-          throw new Error(
-            `The reference ${newName} already exist. Support for merging tags is not implemented yet`
+        // create a new workspace edit to apply
+        const workspaceEdit = new vscode.WorkspaceEdit();
+        // replace content in each backlink with the new name
+        for (let backLink of backLinks) {
+          const contentRange = fileReferenceContentRange(backLink);
+          if (contentRange === undefined) {
+            continue;
+          }
+          workspaceEdit.replace(
+            vscode.Uri.file(backLink.sourceFsPath),
+            contentRange,
+            newName
           );
         }
-        // check that the old document exists
-        if (fs.existsSync(originalFsPath)) {
-          // apply the rename
-          workspaceEdit.renameFile(document.uri, newUri);
+        // rename the file
+        // get the new uri
+        const newUri = vscode.Uri.file(
+          path.resolve(refTargetFsPath, "..", titleToBasename(newName))
+        );
+        // only rename if the file is going to a new place
+        // this can occur if we change the reference name in such a way that
+        // the slugged version normalizes to the same file
+        if (newUri.fsPath !== refTargetFsPath) {
+          // check if the uri already exists
+          // can occur if the user is trying to rename all instances of one reference
+          // to another reference
+          const existingFsPaths = new Set(
+            selectLinkedFileFsPaths(this.store.getState())
+          );
+          const newFsPath = uriFsPath(newUri);
+          // throw an error if the user is merging references (not sure how to support)
+          if (existingFsPaths.has(newFsPath)) {
+            throw new Error(
+              `The reference ${newName} already exist. Support for merging tags is not implemented yet`
+            );
+          }
+          // check that the old document exists
+          if (fs.existsSync(refTargetFsPath)) {
+            // apply the rename
+            workspaceEdit.renameFile(vscode.Uri.file(refTargetFsPath), newUri);
+          }
         }
+        // return the edit
+        return workspaceEdit;
       }
-      // return the edit
-      return workspaceEdit;
     }
     return undefined;
   }
