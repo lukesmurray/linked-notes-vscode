1b57ff0f0a82785b0e7542967577728af3174741
Luke Murray <lukepigeonmail@gmail.com>
2020-07-19 12:52:39 -0400

    fix document link provider and increment version

--- src/reducers/selectDocumentLinksByFsPath.ts
+++ src/reducers/selectDocumentLinksByFsPath.ts
@@ -1,6 +1,6 @@
 import { createObjectSelector } from "reselect-map";
 import * as vscode from "vscode";
-import { isContextFileReference } from "../core/common/typeGuards";
+import { isWikilinkFileReference } from "../core/common/typeGuards";
 import { FileReference } from "../core/common/types";
 import { unistPositionToVscodeRange } from "../core/common/unistPositionToVscodeRange";
 import { isNotNullOrUndefined } from "../utils/util";
@@ -14,7 +14,7 @@ export const selectDocumentLinksByFsPath = createObjectSelector(
   selectFileReferencesByFsPath,
   (allFileReferences): FileReferenceDocumentLink[] =>
     allFileReferences
-      .filter(isContextFileReference)
+      .filter(isWikilinkFileReference)
       .map((ref) => {
         if (ref.node.position === undefined) {
           return undefined;
